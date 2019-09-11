d51a99db59806e31d884cea1680b47aacb1f7b90
Faizan Khan <mathemagician229@gmail.com>
2019-09-11 02:52:22 +0000

    Adding completed task 12.

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 def fizzbuzz():
-    x = 1
-    while (x <= 100):
-        if (x % 15 == 0):
-            print("FizzBuzz", end=" ")
-        elif (x % 3 == 0):
-            print("Fizz", end=" ")
-        elif (x % 5 == 0):
-            print("Buzz", end=" ")
+    for x in range(1, 101):
+        if x % 15 == 0:
+            print("FizzBuzz ", end="")
+        elif x % 3 == 0:
+            print("Fizz ", end="")
+        elif x % 5 == 0:
+            print("Buzz ", end="")
         else:
-            print(x, end=" ")
-        x = x + 1
+            print("{:d} ".format(x), end="")
