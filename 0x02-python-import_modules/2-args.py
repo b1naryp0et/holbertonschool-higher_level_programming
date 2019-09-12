212420a81d8d4deb34ee9bc2fa34af562658e363
Faizan Khan <mathemagician229@gmail.com>
2019-09-12 20:27:32 +0000

    Adding completed task 2.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    argc = len(argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    else:
+        print("{:d} arguments:".format(argc))
+        for x in range(1, argc + 1):
+            print("{:d}: {}".format(x, argv[x]))
