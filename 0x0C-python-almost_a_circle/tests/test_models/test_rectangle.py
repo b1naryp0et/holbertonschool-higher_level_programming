97780cd3cd197fff57a88848368e615ebae83671
Faizan Khan <mathemagician229@gmail.com>
2019-10-24 02:41:17 +0000

    Thanks Brandyn and Mitch for the hack.

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -5,12 +5,14 @@ import unittest
 from models.rectangle import Rectangle
 from models.base import Base
 from models.square import Square
+import pep8
 
-class rectangleTest(unittest.TestCase):
-    """Rectangle class tests"""
 
-    def rectArea(self):
-        """Tests area"""
-
-        e = Rectangle(5, 6)
-        self.assertEqual(e.area(), 30)
+class TestRectangle(unittest.TestCase):
+    """ Class rectangle tests """
+    def test_pep8(self):
+        """ tests prp8 formating """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/rectangle.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style erros (and warnings).")
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -5,14 +5,14 @@ import unittest
 from models.rectangle import Rectangle
 from models.base import Base
 from models.square import Square
+import pep8
 
 
-class squareTests(unittest.TestCase):
-    """Class for testing Square"""
-
-    def sq_area(self):
-        """Area test"""
-
-        e = Square(5)
-        self.assertEqual(e.area(), 25)
-
+class TestRectangle(unittest.TestCase):
+    """ Class rectangle tests """
+    def test_pep8(self):
+        """ tests prp8 formating """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/square.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style erros (and warnings).")
