eec35b97be71dbd59f0d1c62d10bfa6c69fc0ed3
Faizan <mathemagician229@gmail.com>
2020-02-10 21:09:39 -0500

    Adding mostly completed tasks.

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let x = 0;
+  for (let j = 0; j < list.length; j++) {
+    if (list[j] === searchElement) {
+      x = x + 1;
+    }
+  }
+  return x;
+};
